f80942dc964999453c350d9e70eb8457bccbde34
vwajq <86394973+vwajq@users.noreply.github.com>
2023-03-16 13:52:39 +0000

    Edited imuCalibrationTest to test ability to write calibration offset values to EEPROM

--- avionics/ONB Software/test/imuCalibrationTest.cpp
+++ avionics/ONB Software/test/imuCalibrationTest.cpp
@@ -34,7 +34,7 @@
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial) delay(10);
 
   delay(100);
   Serial.println("Calibration filesys test");
@@ -46,41 +46,42 @@ void setup() {
   Serial.print("Has FLASH: "); Serial.println(cal.hasFLASH());
 
   if (! cal.loadCalibration()) {
-    Serial.println("**WARNING** No calibration loaded/found");
+    Serial.println("No calibration loaded/found... will start with defaults");
+  } else {
+    Serial.println("Loaded existing calibration");
   }
+
+  // in uTesla
+  cal.mag_hardiron[0] = -3.35;
+  cal.mag_hardiron[1] = -0.74;
+  cal.mag_hardiron[2] = -40.79;
+
+  // in uTesla
+  cal.mag_softiron[0] = 0.965;
+  cal.mag_softiron[1] = 0.018;
+  cal.mag_softiron[2] = 0.010;  
+  cal.mag_softiron[3] = 0.018;
+  cal.mag_softiron[4] = 0.960;
+  cal.mag_softiron[5] = 0.003;  
+  cal.mag_softiron[6] = 0.010;
+  cal.mag_softiron[7] = 0.003;
+  cal.mag_softiron[8] = 1.080;
+  // Earth total magnetic field strength in uTesla (dependent on location and time of the year),
+  // visit: https://www.ngdc.noaa.gov/geomag/calculators/magcalc.shtml#igrfwmm)
+  cal.mag_field = 45.00; // approximate value for locations along the equator
+
+  // in Radians/s
+  cal.gyro_zerorate[0] = 0.05;
+  cal.gyro_zerorate[1] = -0.01;
+  cal.gyro_zerorate[2] = -0.01;
+
+  if (! cal.saveCalibration()) {
+    Serial.println("**WARNING** Couldn't save calibration");
+  } else {
+    Serial.println("Wrote calibration");    
+  }
+
   cal.printSavedCalibration();
-
-  Serial.println("Calibrations found: ");
-  Serial.print("\tMagnetic Hard Offset: ");
-  for (int i=0; i<3; i++) {
-    Serial.print(cal.mag_hardiron[i]); 
-    if (i != 2) Serial.print(", ");
-  }
-  Serial.println();
-  
-  Serial.print("\tMagnetic Soft Offset: ");
-  for (int i=0; i<9; i++) {
-    Serial.print(cal.mag_softiron[i]); 
-    if (i != 8) Serial.print(", ");
-  }
-  Serial.println();
-
-  Serial.print("\tMagnetic Field Magnitude: ");
-  Serial.println(cal.mag_field);
-
-  Serial.print("\tGyro Zero Rate Offset: ");
-  for (int i=0; i<3; i++) {
-    Serial.print(cal.gyro_zerorate[i]); 
-    if (i != 2) Serial.print(", ");
-  }
-  Serial.println();
-
-  Serial.print("\tAccel Zero G Offset: ");
-  for (int i=0; i<3; i++) {
-    Serial.print(cal.accel_zerog[i]); 
-    if (i != 2) Serial.print(", ");
-  }
-  Serial.println();
 }
 
 void loop() {
