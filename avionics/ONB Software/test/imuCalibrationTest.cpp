3d26e2d5bfde3312f2a2cf3c62a6c5f604d6e36c
vwajq <86394973+vwajq@users.noreply.github.com>
2023-03-16 13:39:29 +0000

    Added Sensor Calibration library and edited imuCalibrationTest to test for calibration storage

---
+++ avionics/ONB Software/test/imuCalibrationTest.cpp
@@ -0,0 +1,88 @@
+// The purpose of this test is to determine the appropriate calibration settings for the IMU to be stored on the BPI Leaf S3's FLASH
+
+// #include "imu.h"
+
+// void setup()
+// {
+//     Serial.begin(115200);
+//     while (!Serial);
+
+//     imuSetup();
+// }
+
+// void loop()
+// {
+//     imuGetData();
+//     Serial.println("IMU Data");
+//     Serial.printf("Temperature: %f\n", imuData.temperature);
+//     Serial.printf("Acceleration (x, y, z): %f %f %f\n", imuData.accelX, imuData.accelY, imuData.accelZ);
+//     Serial.printf("Angle (x, y, z): %f %f %f\n\n", imuData.gyroX, imuData.gyroY, imuData.gyroZ);
+
+//     Serial.flush();
+
+//     delay(500);
+// }
+
+#include "Adafruit_Sensor_Calibration.h"
+
+// select either EEPROM or SPI FLASH storage:
+#ifdef ADAFRUIT_SENSOR_CALIBRATION_USE_EEPROM
+  Adafruit_Sensor_Calibration_EEPROM cal;
+#else
+  Adafruit_Sensor_Calibration_SDFat cal;
+#endif
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  delay(100);
+  Serial.println("Calibration filesys test");
+  if (!cal.begin()) {
+    Serial.println("Failed to initialize calibration helper");
+    while (1) yield();
+  }
+  Serial.print("Has EEPROM: "); Serial.println(cal.hasEEPROM());
+  Serial.print("Has FLASH: "); Serial.println(cal.hasFLASH());
+
+  if (! cal.loadCalibration()) {
+    Serial.println("**WARNING** No calibration loaded/found");
+  }
+  cal.printSavedCalibration();
+
+  Serial.println("Calibrations found: ");
+  Serial.print("\tMagnetic Hard Offset: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(cal.mag_hardiron[i]); 
+    if (i != 2) Serial.print(", ");
+  }
+  Serial.println();
+  
+  Serial.print("\tMagnetic Soft Offset: ");
+  for (int i=0; i<9; i++) {
+    Serial.print(cal.mag_softiron[i]); 
+    if (i != 8) Serial.print(", ");
+  }
+  Serial.println();
+
+  Serial.print("\tMagnetic Field Magnitude: ");
+  Serial.println(cal.mag_field);
+
+  Serial.print("\tGyro Zero Rate Offset: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(cal.gyro_zerorate[i]); 
+    if (i != 2) Serial.print(", ");
+  }
+  Serial.println();
+
+  Serial.print("\tAccel Zero G Offset: ");
+  for (int i=0; i<3; i++) {
+    Serial.print(cal.accel_zerog[i]); 
+    if (i != 2) Serial.print(", ");
+  }
+  Serial.println();
+}
+
+void loop() {
+
+}
