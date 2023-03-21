7a1f1fcf297e40b2476f880ce52cd330f6d91690
vwajq <86394973+vwajq@users.noreply.github.com>
2023-03-21 11:10:27 +0000

    Edited 'gpsTest.' file to display elapsed time when first fix is established

---
+++ avionics/ONB Software/test/gpsTest.cpp
@@ -0,0 +1,28 @@
+#include "gps.h"
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial);
+
+    gpsSetup();
+}
+
+void loop()
+{
+    gpsGetData();
+    Serial.println("GPS Data");
+    Serial.printf("Longitude: %" PRId32 "\n", gpsData.longitude);
+    Serial.printf("Latitude: %" PRId32 "\n", gpsData.latitude);
+    Serial.printf("Satellites Detected: %" PRIu8 "\n\n", gpsData.satellites);
+
+    if (gpsData.satellites > 0)
+    {
+        Serial.print(millis() / 1000);
+        Serial.println(" seconds");
+        while(true);
+    }
+    
+
+
+}
