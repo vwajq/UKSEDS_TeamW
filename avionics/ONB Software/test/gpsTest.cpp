a2b8ff0b6dbf4b535a6b0a44957cd67f2e4815ea
vwajq <86394973+vwajq@users.noreply.github.com>
2023-03-22 04:59:31 +0000

    Added some comments

--- avionics/ONB Software/test/gpsTest.cpp
+++ avionics/ONB Software/test/gpsTest.cpp
@@ -16,13 +16,11 @@ void loop()
     Serial.printf("Latitude: %" PRId32 "\n", gpsData.latitude);
     Serial.printf("Satellites Detected: %" PRIu8 "\n\n", gpsData.satellites);
 
+    // Prints out the TTFF and stops program execution
     if (gpsData.satellites > 0)
     {
         Serial.print(millis() / 1000);
         Serial.println(" seconds");
         while(true);
     }
-    
-
-
 }
