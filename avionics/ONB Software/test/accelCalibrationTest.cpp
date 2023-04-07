861986e6cb04bdeb4b9d449fc46ed4bb50f3ecfd
vwajq <86394973+vwajq@users.noreply.github.com>
2023-04-07 22:45:01 +0100

    Finalized accelerometer calibration code (Simple Version)

--- avionics/ONB Software/test/accelCalibrationTest.cpp
+++ avionics/ONB Software/test/accelCalibrationTest.cpp
@@ -6,9 +6,9 @@ int settlingTime = 4;
 int calibrationTime = 60;
 int endTime;
 
-float accelOffsetX = 0;
-float accelOffsetY = 0;
-float accelOffsetZ = 0;
+float accelOffsetX;
+float accelOffsetY;
+float accelOffsetZ;
 int numPoints = 0;
 
 float xSum = 0;
@@ -20,7 +20,7 @@ int flag;
 
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
     while (!Serial);
 
     imuSetup();
@@ -38,32 +38,32 @@ void setup()
     flag = 0;
     
 
-    Serial.println("Beginning to calibrate acceleration in x-axis");
+    Serial.println("Beginning to calibrate acceleration pt. 1");
     endTime = millis() + calibrationTime*1000;
     while (millis() < endTime)
     {
-        imuGetData();
         numPoints++;
+        imuGetData();
 
-        // accelOffsetX = 9.81 - imuData.accelX;
-        // accelOffsetY = 9.81 - imuData.accelY;
-        accelOffsetZ = 9.81 - imuData.accelZ;
+        accelOffsetX = imuData.accelX - 9.806;
+        // accelOffsetY = imuData.accelY - 9.806;
+        // accelOffsetZ = imuData.accelZ - 9.806;
 
-        xSum += 9.81;
+        xSum += 9.806;
 
-        // ySum += accelOffsetX;
+        ySum += accelOffsetX;
         // ySum += accelOffsetY;
-        ySum += accelOffsetZ;
+        // ySum += accelOffsetZ;
 
-        xSqSum += 9.81;
+        xSqSum += 9.806 * 9.806;
         
-        // xySum += 9.81 * accelOffsetX;
-        // xySum += 9.81 * accelOffsetY;
-        xySum += 9.81 * accelOffsetZ;
+        xySum += 9.806 * accelOffsetX;
+        // xySum += 9.806 * accelOffsetY;
+        // xySum += 9.806 * accelOffsetZ;
 
         if (numPoints % 100 == 0)
         {
-            Serial.printf("Calibrating acceleration (9.81 m/s^2) pt. 1... %d points completed\n", numPoints);
+            Serial.printf("Calibrating acceleration (Acceleration: 9.81 m/s^2) pt. 1... %d points completed\n", numPoints);
         }
     }
 
@@ -79,28 +79,28 @@ void setup()
     endTime = millis() + calibrationTime*1000;
     while (millis() < endTime)
     {
-        imuGetData();
         numPoints++;
+        imuGetData();
 
-        // accelOffsetX = -9.81 - imuData.accelX;
-        // accelOffsetY = -9.81 - imuData.accelY;
-        accelOffsetZ = -9.81 - imuData.accelZ;
+        accelOffsetX = imuData.accelX + 9.806;
+        // accelOffsetY = imuData.accelY + 9.806;
+        // accelOffsetZ = imuData.accelZ + 9.806;
 
-        xSum += -9.81;
+        xSum += (-9.806);
 
-        // ySum += accelOffsetX;
+        ySum += accelOffsetX;
         // ySum += accelOffsetY;
-        ySum += accelOffsetZ;
+        // ySum += accelOffsetZ;
 
-        xSqSum += (-9.81) * (-9.81);
+        xSqSum += (-9.806) * (-9.806);
 
-        // xySum += -9.81 * accelOffsetX;
-        // xySum += -9.81 * accelOffsetY;
-        xySum += -9.81 * accelOffsetZ;
+        xySum += -9.806 * accelOffsetX;
+        // xySum += -9.806 * accelOffsetY;
+        // xySum += (-9.806) * accelOffsetZ;
 
         if (numPoints % 100 == 0)
         {
-            Serial.printf("Calibrating acceleration (-9.81 m/s^2) pt. 2... %d points completed\n", numPoints);
+            Serial.printf("Calibrating acceleration (Acceleration: -9.81 m/s^2) pt. 2... %d points completed\n", numPoints);
         }
     }
 
@@ -113,49 +113,50 @@ void setup()
     }while (flag != 1);
     flag = 0;
     
-    Serial.println("Beginning to calibrate acceleration (0 m/s^2) pt. 3");
+    Serial.println("Beginning to calibrate acceleration pt. 3");
     endTime = millis() + calibrationTime*1000; 
     while (millis() < endTime)
     {
-        imuGetData();
         numPoints++;
+        imuGetData();
 
-        // accelOffsetX = imuData.accelX;
+        accelOffsetX = imuData.accelX;
         // accelOffsetY = imuData.accelY;
-        accelOffsetZ = imuData.accelZ;
+        // accelOffsetZ = imuData.accelZ;
 
         xSum += 0;
 
-        // ySum += accelOffsetX;
+        ySum += accelOffsetX;
         // ySum += accelOffsetY;
-        ySum += accelOffsetZ;
+        // ySum += accelOffsetZ;
 
         xSqSum += 0 * 0;
 
-        // xySum += 0 * accelOffsetX;
+        xySum += 0 * accelOffsetX;
         // xySum += 0 * accelOffsetY;
-        xySum += 0 * accelOffsetZ;
+        // xySum += 0 * accelOffsetZ;
 
         if (numPoints % 100 == 0)
         {
-            Serial.printf("Calibrating acceleration (0 m/s^2) pt. 3... %d points completed\n", numPoints);
+            Serial.printf("Calibrating acceleration (Acceleration: 0 m/s^2) pt. 3... %d points completed\n", numPoints);
         }
     }
 
     Serial.printf("Calibration complete! %d points completed in total\n", numPoints);
 
-    float m = (numPoints * xySum - (xSum * ySum)) / ((numPoints * xSqSum) - (xSum) * (xSum));
+    float m = ((numPoints * xySum) - (xSum * ySum)) / ((numPoints * xSqSum) - (xSum * xSum));
     float c = (ySum - (m * xSum)) / numPoints;
-    Serial.printf("m: %f\n", m);
+
+    Serial.printf("m: %f\n", m*2);
     Serial.printf("c: %f\n", c);
     while (true);
 }
 
 void loop()
 {
-    // Prints out the IMU values (Acceleration in m/s^2, Angular Rate in rad/s)
-    // Serial.println("IMU Data");
-    // // Serial.printf("Acceleration (x, y, z): %f %f %f\n", imuData.accelX, imuData.accelY, imuData.accelZ);
-    // Serial.printf("Angle (x, y, z): %f %f %f\n\n", imuData.gyroX - 0.004169, imuData.gyroY - 0.003486, imuData.gyroZ + 0.013477);
-    // delay(1000);
+    // Prints out the IMU values (Acceleration in m/s^2)
+    imuGetData();
+    Serial.println("IMU Data");
+    Serial.printf("Acceleration (x, y, z): %f %f %f\n", imuData.accelX, imuData.accelY, imuData.accelZ);
+    delay(200);
 }
