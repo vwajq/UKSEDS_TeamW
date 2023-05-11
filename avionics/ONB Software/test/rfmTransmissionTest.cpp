8d6643ed87bf9f85b83cbc51e6e4207247ca65e1
vwajq <86394973+vwajq@users.noreply.github.com>
2023-05-11 09:07:17 +0100

    Updated rfmTransmissionTest.cpp to print out transmission time

--- avionics/ONB Software/test/rfmTransmissionTest.cpp
+++ avionics/ONB Software/test/rfmTransmissionTest.cpp
@@ -6,9 +6,19 @@ void setup()
     while (!Serial);
 
     rfmTransmitterSetup();
+
+    for (int i = 0; i < BYTES_TO_TRANSMIT; i++)
+    {
+        byteArrTransmit[i] = i;
+        unsigned long preTransmit = micros();
+        rfmTransmit(byteArrTransmit);
+        unsigned long postTransmit = micros();
+
+        Serial.printf("Transmission time (us): %d", postTransmit - preTransmit);
+    }
 }
 
 void loop()
 {
-    rfmTransmit();
+
 }
